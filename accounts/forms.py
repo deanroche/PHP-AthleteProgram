96657a34f5b328c7bfeb775970620dcb20e87bf0
auxfuse <aux.fuse@yahoo.com>
2020-09-05 17:48:35 +0100

    Accounts app added with all appropraite function views and templates

---
+++ accounts/forms.py
@@ -0,0 +1,117 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.core.validators import ValidationError
+
+
+# Forms
+class UserRegistrationForm(UserCreationForm):
+    """
+    User registration form, extending on the UserCreationForm with
+    cleaned_email validation in play and disabling the default helper text
+    that is displayed in tandem with the default form. With html attributes
+    set via widgets handlers for same.
+    """
+    first_name = forms.CharField(
+        required=True,
+        label='',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter First Name'
+        })
+    )
+    last_name = forms.CharField(
+        required=True,
+        label='',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter Last Name'
+        })
+    )
+    username = forms.CharField(
+        required=True,
+        label='',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter Username'
+        })
+    )
+    email = forms.EmailField(
+        required=True,
+        label='',
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter Valid Email'
+        })
+    )
+    password1 = forms.CharField(
+        required=True,
+        label='',
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter Password'
+        })
+    )
+    password2 = forms.CharField(
+        required=True,
+        label='',
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Re-Enter Password'
+        })
+    )
+
+    class Meta:
+        model = User
+        fields = [
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password1',
+            'password2'
+        ]
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError(
+                "This email is already coupled to an account."
+            )
+        return email
+
+    """
+    Disable help text of UserCreationForm (answer got from: 
+    'https://stackoverflow.com/questions/13202845/removing-help-text-from-django-usercreateform'
+    & set default autofocus to first_name instead of the default field of 
+    username.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+        self.fields['first_name'].widget.attrs['autofocus'] = True
+        self.fields['username'].widget.attrs['autofocus'] = False
+
+
+class UserLoginForm(forms.Form):
+    """
+    User login with html attributes set via widgets handlers for same.
+    """
+    username = forms.CharField(
+        required=True,
+        label='',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter Username',
+            'autofocus': 'True'
+        })
+    )
+    password = forms.CharField(
+        required=True,
+        label='',
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter Password'
+        })
+    )
---
+++ accounts/views.py
@@ -0,0 +1,93 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import auth
+from django.contrib import messages
+from .forms import UserRegistrationForm, UserLoginForm
+
+
+# Views
+def register(request):
+    """
+    Register User if all checks done in form validation are passed.
+    If successful, return user to Index with notification of success, else
+    display error on register.html template.
+    If User already logged in and URL injection used to navigate to
+    register.html, redirect user to Index with error notification of same.
+    :param request:
+    :return:
+    """
+    if request.method == 'POST':
+        reg_form = UserRegistrationForm(request.POST)
+
+        if reg_form.is_valid():
+            user = reg_form.save()
+            auth.login(request, user)
+            messages.success(request, 'You are now registered & logged in.')
+            return redirect('index')
+
+    elif request.user.is_authenticated:
+        messages.error(request, 'You are logged in already!')
+        return redirect('index')
+
+    else:
+        reg_form = UserRegistrationForm()
+
+    context = {
+        'register_page': 'active',
+        'form': reg_form
+    }
+
+    return render(request, 'accounts/register.html', context)
+
+
+def login(request):
+    """
+    Log user in and redirect to Index page with success message.
+    If user already logged in and attempt made to navigate to login page from
+    URL injection display error message and redirect back to Index.
+    :param request:
+    :return:
+    """
+    if request.method == 'POST':
+        login_form = UserLoginForm(request.POST)
+        if login_form.is_valid():
+            user = auth.authenticate(
+                username=request.POST['username'],
+                password=request.POST['password']
+            )
+            if user:
+                auth.login(request, user)
+                messages.success(request, 'Successfully logged in.')
+                return redirect('index')
+            else:
+                messages.error(request, 'Invalid Login details!')
+                return redirect('login')
+    else:
+        login_form = UserLoginForm()
+
+    context = {
+        'login-page': 'active',
+        'form': login_form
+    }
+    return render(request, 'accounts/login.html', context)
+
+
+def logout(request):
+    """
+    Logout User from current session & display success message of same.
+    :param request:
+    :return:
+    """
+    logout(request)
+    messages.success(request, 'Thank you! You have successfully logged out.')
+    return redirect('index')
+
+
+def dashboard(request):
+    """
+    Navigate User to dashboard containing each day navigation for the PHP
+    Athlete program.
+    :param request:
+    :return:
+    """
+    return render(request, 'accounts/dashboard.html')
+
