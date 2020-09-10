70f4daa11e8a4ce02b136c512f1b5d65f80ba45a
auxfuse <aux.fuse@yahoo.com>
2020-09-10 22:04:16 +0100

    workouts app setup with Workout models added

--- workouts/admin.py
+++ workouts/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Workout
 
-# Register your models here.
+# Models
+admin.site.register(Workout)
--- workouts/models.py
+++ workouts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Models
+class Workout(models.Model):
+    """
+    Model to define the fields required to create workouts per day.
+    """
+    title = models.CharField(max_length=150)
+    coach = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(default=timezone.now)
+    part_a = models.TextField()
+    part_b = models.TextField()
+    part_c = models.TextField()
+    coach_comments = models.TextField()
+
+    def __str__(self):
+        return self.title
