ac3b8dcf8490b967ac11c9b7ebf6cac6d468688a
auxfuse <aux.fuse@yahoo.com>
2020-09-18 20:15:00 +0100

    Removed workout-view ability and amalgamated to dashboard

--- workouts/urls.py
+++ workouts/urls.py
@@ -1,7 +1,1 @@
-from django.urls import path
-from . import views
 
-
-urlpatterns = [
-    path('workout-view/<int:workout_id>', views.workout, name='workout-view')
-]
--- workouts/views.py
+++ workouts/views.py
@@ -1,19 +1,1 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Workout
 
-
-# Views.
-def workout(request, workout_id):
-    """
-    View Workout and render same to user.
-    :param request:
-    :param workout_id:
-    :return:
-    """
-    workout = get_object_or_404(Workout, pk=workout_id)
-
-    context = {
-        'workout': workout
-    }
-
-    return render(request, 'workouts/workout-view.html', context)
