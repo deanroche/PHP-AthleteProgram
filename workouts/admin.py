bcbc3bedc26907643d3e27dfeb0c4c357661a713
auxfuse <aux.fuse@yahoo.com>
2020-09-30 16:10:40 +0100

    Restructured Workout table header bar to include more columns and provided appropriate styling to suit

--- workouts/admin.py
+++ workouts/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Workout
 
+
+class WorkoutAdmin(admin.ModelAdmin):
+    list_display = ('title', 'workout_date', 'category')
+    list_display_links = ('title', 'workout_date')
+    search_fields = ('title',)
+    list_per_page = 20
+    list_filter = ('category',)
+
+
 # Models
-admin.site.register(Workout)
+admin.site.register(Workout, WorkoutAdmin)
